320d44f6bb59d679efa11bc0e7c3da5ab97bc9aa
milen1017 <mn.krumov@gmail.com>
2023-11-14 23:40:31 +0000

    Secured environment variables in .env file

--- server/index.js
+++ server/index.js
@@ -4,13 +4,22 @@ const { default: mongoose } = require("mongoose");
 const User = require("./models/User");
 const bcrypt = require('bcrypt');
 const app = express();
+require('dotenv').config();
+
+const { DB_CONNECTION } = process.env;
 
 app.use(cors());
 app.use(express.json());
 
- mongoose.connect(
-	"mongodb+srv://softuni-react-project:TyR8e72pYssRxegY@cluster0.la2gkzx.mongodb.net/?retryWrites=true&w=majority"
-);
+
+
+mongoose.connect(process.env.DB_CONNECTION)
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to database:', error);
+  });
 
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
