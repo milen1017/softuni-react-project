e4ac75418e7fbf90a87b090a96f5cb275dc27a49
milen1017 <mn.krumov@gmail.com>
2023-11-15 20:12:41 +0000

    added login functionality

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
+import {Navigate} from "react-router-dom";
+
+
 const Login = () => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [redirect,setRedirect] = useState(false);
+	async function onLogin(e) {
+		e.preventDefault();
+		const response = await fetch("http://localhost:3000/login", {
+			method: "POST",
+			body: JSON.stringify({ username, password }),
+			headers: { "Content-Type": "application/json" },
+			credentials: "include",
+		});
+		if (response.ok) {
+			setRedirect(true);
+			
+		} else {
+			alert("Wrong credentials");
+		}
+	}
+	if (redirect) {
+		return <Navigate to={'/'} />
+	  }
+
 	return (
-		<form action="">
-			<input type="email" placeholder="email" />
-			<input type="password" placeholder="password" />
+		<form className="login" onSubmit={onLogin}>
+			<input
+				type="username"
+				placeholder="username"
+				value={username}
+				onChange={(ev) => {
+					setUsername(ev.target.value);
+				}}
+			/>
+			<input
+				type="password"
+				placeholder="password"
+				value={password}
+				onChange={(ev) => {
+					setPassword(ev.target.value);
+				}}
+			/>
 			<button type="submit">Login</button>
 		</form>
 	);
--- server/index.js
+++ server/index.js
@@ -2,39 +2,58 @@ const express = require("express");
 const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 const User = require("./models/User");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const crypto = require('crypto');
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 const { DB_CONNECTION } = process.env;
+const secretKey = crypto.randomBytes(32).toString('base64');
 
-app.use(cors());
+app.use(cors({credentials:true,origin:"http://localhost:5173"}));
 app.use(express.json());
 
+mongoose
+	.connect(process.env.DB_CONNECTION)
+	.then(() => {
+		console.log("Connected to database");
+	})
+	.catch((error) => {
+		console.error("Error connecting to database:", error);
+	});
 
+app.post("/register", async (req, res) => {
+	const { username, password } = req.body;
+	const hashedPassword = await bcrypt.hash(password, 10);
+	try {
+		const userDoc = await User.create({
+			username,
+			hashedPassword,
+		});
+		res.json(userDoc);
+	} catch (e) {
+		console.log(e);
+		res.status(400).json(e);
+	}
+});
 
-mongoose.connect(process.env.DB_CONNECTION)
-  .then(() => {
-    console.log('Connected to database');
-  })
-  .catch((error) => {
-    console.error('Error connecting to database:', error);
-  });
+app.post("/login", async (req, res) => {
+	const { username, password } = req.body;
+	const userData = await User.findOne({ username });
 
-app.post('/register', async (req,res) => {
-    const {username,password} = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    try{
-      const userDoc = await User.create({
-        username,
-        hashedPassword
-      });
-      res.json(userDoc);
-    } catch(e) {
-      console.log(e);
-      res.status(400).json(e);
-    }
-  });
+	const passOk = bcrypt.compareSync(password, userData.hashedPassword);
+	if (passOk) {
+		jwt.sign({ username, id: userData._id }, secretKey, {}, (err, token) => {
+			if (err) throw err;
+			res.cookie("token", token).json({
+				id: userData._id,
+				username,
+			});
+		});
+	} else {
+		res.status(400).json("Wrong credentials");
+	}
+});
 
-app.listen(3000);
-
+app.listen(3000, console.log("Server is running at port 3000"));
