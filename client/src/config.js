1eb69d804f85b8b4b6dc43b8c70ae71c93f71cfd
milen1017 <mn.krumov@gmail.com>
2023-12-08 17:43:42 +0000

    url string change

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http://localhost:3000' || "softuni-react-project-backend-milen1017s-projects.vercel.app";
+const BASE_URL = "https://softuni-react-project-backend-milen1017s-projects.vercel.app";
 
 export default BASE_URL;
