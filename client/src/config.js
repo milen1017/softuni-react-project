1e819c14c3b6f6c06cae0ba9b6ea31cf15fbf56d
milen1017 <mn.krumov@gmail.com>
2023-12-08 17:36:39 +0000

    add connection string

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http://localhost:3000' || "https://softuni-react-project-sigma.vercel.app/server";
+const BASE_URL = 'http://localhost:3000' || "softuni-react-project-backend-milen1017s-projects.vercel.app";
 
 export default BASE_URL;
