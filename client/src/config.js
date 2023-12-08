04e4aa360adc2afe5d30cd7b3a161b36d75d3e67
milen1017 <mn.krumov@gmail.com>
2023-12-08 20:00:47 +0000

    revert 9762ca27c15c5b637f271b07955d15d592e84b9e

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,6 @@
-const BASE_URL = "https://softuni-react-project-backend-milen1017s-projects.vercel.app";
+const BASE_URL = "http://localhost:3000";
+// const BASE_URL = "https://softuni-react-project-backend-milen1017s-projects.vercel.app";
+
+
 
 export default BASE_URL;
