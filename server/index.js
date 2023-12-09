0ec61c79bf74c521e32561767281dec20536595d
milen1017 <mn.krumov@gmail.com>
2023-12-09 18:34:20 +0000

    add allow cors handler

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const app = express();
@@ -8,42 +7,60 @@ require('dotenv').config();
 const { DB_CONNECTION, PORT } = process.env;
 
 const port = process.env.PORT || 3001;
-// port var for render deploy
 
 const postRoutes = require('./routes/postRoutes');
 const authRoutes = require('./routes/authRoutes');
 
+// Your allowCors middleware function
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+// Your route handlers
+const handler = (req, res) => {
+  const d = new Date();
+  res.end(d.toString());
+};
+
+// Wrap your handlers with allowCors middleware
+const handlerWithCors = allowCors(handler);
+
 app.use(express.json());
-app.use(
-	cors({
-		credentials: true,
-		origin: [
-			'http://localhost:5173',
-			'https://softuni-react-project-sigma.vercel.app',
-		],
-		methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	})
-);
 app.use(cookieParser());
 
 mongoose
-	.connect(`${DB_CONNECTION}`)
-	.then(() => {
-		console.log('Connected to database');
-	})
-	.catch((error) => {
-		console.error('Error connecting to database:', error);
-	});
+  .connect(`${DB_CONNECTION}`)
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to database:', error);
+  });
 
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
-app.use('/auth', authRoutes);
-
-app.use('/posts', postRoutes);
+app.use('/auth', allowCors(authRoutes)); // Wrap authRoutes with allowCors middleware
+app.use('/posts', allowCors(postRoutes)); // Wrap postRoutes with allowCors middleware
 
 app.listen(PORT, () => {
-	console.log(`Server is running at port ${PORT}`);
+  console.log(`Server is running at port ${PORT}`);
 });
